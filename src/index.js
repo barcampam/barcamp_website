196d909632488f94cbfcae125ac213158b822816
davitk <dkroyan@gmail.com>
2017-05-11 19:34:29 +0400

    [FEAT](View) Create all pages(blank) and add to route

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import {Router,browserHistory} from "react-router"
-import Pomise from "redux-promise"
+import Promise from "redux-promise"
 
 import Routes from "./router"
 import reducers from './reducers/index';
 
-const createStoreWithMiddleware = applyMiddleware(Pomise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(Promise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <Router history={browserHistory} routes={Routes}/>
     </Provider>
-    , document.querySelector('.initial'));
+    , document.querySelector('.all-content-wrapper'));
