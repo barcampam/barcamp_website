b98dfd36de38a4631ddd6814346da7a3f83b12d5
davitk <dkroyan@gmail.com>
2017-05-12 19:35:25 +0400

    [FEAT](view) Add home page top section

--- src/components/app.js
+++ src/components/app.js
@@ -6,13 +6,10 @@ import Footer from "./footer"
 export default class App extends Component {
     render() {
         return (
-
             <div >
-                <div className="container">
                     <Header/>
                     {this.props.children}
                     <Footer/>
-                </div>
             </div>
         );
     }
--- src/containers/content.js
+++ src/containers/content.js
@@ -1,12 +1,48 @@
-import React,{Component} from "react"
+import React, {Component} from "react"
 
 class Content extends Component{
-
-
     render(){
         return(
             <div>
-                Content axasxasxasxasxaxasxasxasx
+                <section className="home-top">
+                    <div className="social-links">
+                        <a href=""><img src={require('../../public/img/ic_facebook_share.png')} /></a><br />
+                        <a href=""><img src={require('../../public/img/ic_twtiter_share.png')} /></a><br />
+                        <a href=""><img src={require('../../public/img/ic_instagram_share.png')} /></a>
+                    </div>
+                    <div className="barcamp-hashtag">
+                        <h3>#barcampevn17</h3>
+                    </div>
+                    <div className="light-bulb">
+                        <img src={require('../../public/img/light-bulb.png')} />
+                    </div>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-1 col-md-1 col-sm-1"></div>
+                            <div className="col-lg-7 col-md-7 col-sm-7 col-xs-12 about-event">
+                                <h2>May 27-28</h2>
+                                <p>The most wanted IT and<br />
+                                    Media event in Armenia<br />
+                                    coming in the soon
+                                </p>
+                            </div>
+                            <div className="col-lg-4 col-md-4 col-sm-4"></div>
+                        </div>
+                    </div>
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-lg-8 col-md-8 col-sm-8"></div>
+                            <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12 app-links">
+                                <a target="_blank" href="http://www.hexdivision.com/">
+                                    <img src={require('../../public/img/googleplay.png')} />
+                                </a>
+                                <a target="_blank" href="http://www.hexdivision.com/">
+                                    <img src={require('../../public/img/appstore.png')} />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         )
     }
