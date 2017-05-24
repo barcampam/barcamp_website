623048d83146120451552c4c8bff1b575552ff75
Nver Abgaryan <abgaryannver1993@gmail.com>
2017-05-24 16:50:38 +0400

    [FIX](app) Chnage speakers count

--- src/reducers/SpeakerReducer.js
+++ src/reducers/SpeakerReducer.js
@@ -4,7 +4,7 @@ import {SPEAKER_TYPE} from "../actions/SpeakerAction"
 export default function (state = {}, action) {
     switch (action.type) {
         case SPEAKER_TYPE:
-            return action.payload.data.splice(0,8)
+            return action.payload.data.splice(8,8)
         default:
             return state
     }
