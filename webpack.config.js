b5cdd5e87dc2644ec891c7b1075f7cd1ae9fc355
Nver Abgaryan <abgaryannver1993@gmail.com>
2017-06-12 20:03:52 +0400

    [FIX](project): add babel as a dependency

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         ]
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ['*', '.js', '.jsx']
     },
     devServer: {
         historyApiFallback: true,
